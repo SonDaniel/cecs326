5dee7e3321e9804a7d502e70063d13aaf6b6d2bf
Daniel Son <sondaniel96@gmail.com>
2017-02-14 15:04:27 -0800

    finished assignment

--- assignment_1.cpp
+++ assignment_1.cpp
@@ -5,20 +5,41 @@ Assignment 1 - Dynamic Memory Allocation
 */
 #include <iostream>
 #include <stdlib.h>
-#include <list>
+#include <vector>
+#include <new>
 using namespace std;
 
 /***********************************************************************/
 //   MemoryBlockTable class purpose is to manage dynamic memory space. //
 /***********************************************************************/
+/*
+    MemoryBlockTable()   : Constructor function that will initialize a space 
+                           variable to 118 and set mem array first 10 index 
+                           to true.
+
+    ~MemoryBlockTable()  : Destructor function that will delete MemoryBlockTable
+
+    void setSpace(int x) : Function will allow user to set variable space
+
+    int getSpace()       : Function will grab and return int space variable
+
+    void allocateMemory(int * pageTable, int size ) : 
+                           Function will take pageTable pointer and allocate index of memory
+                           to the int array and set mem array from false to true. Parameter 
+                           size is the size of the pageTable.
+
+    void deallocateMemory(int * pageTable, int size):
+                           Function will take pageTable pointer and deallocate mem array using 
+                           int array pageTable stored. Parameter size is the size of the pageTable.
+*/
 class MemoryBlockTable {
     public:
         MemoryBlockTable();     //constructor
         ~MemoryBlockTable();    //destructor
         void setSpace(int x) {space = x;}
         int getSpace() {return space;}
-        void allocateMemory(bool ** pageTable, int size);
-        void deallocateMemory(bool ** pageTable, int size);
+        void allocateMemory(int * pageTable, int size);
+        void deallocateMemory(int * pageTable, int size);
 
     private:
         int space;
@@ -28,38 +49,36 @@ class MemoryBlockTable {
 //Member function definition Constructor 
 MemoryBlockTable::MemoryBlockTable() { 
     space = 118; //initialize with first 10 slots occupied
-    for(int ii = 0; ii < 10; ii++) { 
+    for(int ii = 0; ii < 10; ++ii) { 
         mem[ii] = true;
     }
     cout << "Memory Block Table Constructed" << endl;
 }
 
+//Member function definition Destructor
 MemoryBlockTable::~MemoryBlockTable() {
     cout << "Memory Block Table Destructed." << endl;
 }
 
-void MemoryBlockTable::allocateMemory(bool ** pageTable, int size) {
-    for(int ii = 0, jj = 0; ii < 128 && jj < size; ii++) {
+//Member function definition allocationMemory
+void MemoryBlockTable::allocateMemory(int * pageTable, int size) {
+    for(int ii = 0, jj = 0; ii < 128 && jj < size; ++ii) {
         if(!mem[ii]) {
-            pageTable[jj++] = &mem[ii];
+            pageTable[jj] = ii; //initialize memory location
+            jj++; //increment counter
             mem[ii] = true; //Set memory to used
         }
     }
-
-    for(int ii = 0; ii < 128; ii++) {
-        cout << &mem[ii] << " : " << mem[ii] << endl;
-    }
 }
 
-void MemoryBlockTable::deallocateMemory(bool ** pageTable, int size) {
-    for(int ii = 0; ii < 128; ii++) {
-        for(int jj = 0; jj < size; jj++) {
-            if(pageTable[jj] == &mem[ii]) {
-                mem[ii] = false; //Set memory to free
-                delete pageTable[jj]; //delete pointer
-            }
-        }
+//Member function definition deallocateMemory
+void MemoryBlockTable::deallocateMemory(int * pageTable, int size) {
+    for(int ii = 0; ii < size; ++ii) {
+        mem[pageTable[ii]] = false; //Set memory to free
     }
+    cout << space << endl;
+    space = space + size; //update space variable
+    cout << space << endl;
 }
 /***********************************************************************/
 //                  End of MemoryBlockTable Class                      //
@@ -69,48 +88,68 @@ void MemoryBlockTable::deallocateMemory(bool ** pageTable, int size) {
 /*******************************************************************************/
 //  ProcessControlBlock class purpose is to manage a process with a page table //
 /******************************************************************************/
+/*
+    ProcessControlBlock(int id, int size) : Constructor function which takes in Parameters 
+                                            id which is a unique id for PID. Size for the 
+                                            memory size the PCB requires.
+
+    ~ProcessControlBlock()                : Destructor function which will delete PageTable
+                                            pointer to retrieve back memory.
+
+    void initializePageTable(int size)    : Function will take in parameter of size which is
+                                            used to dynamically initialize the pageTable size.
+
+    int * getPageTable()                  : Function that returns the pointer of the pageTable.
+
+    int getPID()                          : Function will return a integer of the PID.
+
+    int getSize()                         : Function will return a integer of the size of the process.
+*/
 class ProcessControlBlock {
     public:
         ProcessControlBlock(int id, int size);  //constructor
         ~ProcessControlBlock();      //destructor
         void initializePageTable(int size); //Create PageTable function
-        bool ** getPageTable();
+        int * getPageTable();
         int getPID();
         int getSize();
 
     private:
         int pid;            //unique process ID
         int processSize;    //Size of required memory blocks
-        bool ** pageTable; //Page table
+        int * pageTable; //Page table
 };
 
 //Member function definition Constructor
 ProcessControlBlock::ProcessControlBlock(int id, int size) {
     pid = id;
     processSize = size;
+    pageTable = NULL;
     cout << "Process Control Block #" << pid << " Constructed." << endl;
 }
 
+//Member function definition Destructor
 ProcessControlBlock::~ProcessControlBlock() {
-    for(int ii = 0; ii < processSize; ii++) {
-        delete pageTable[ii]; //delete pageTable content
-    }
-    delete pageTable; //delete pageTable
+    delete [] pageTable; //delete pageTable
     cout << "Process Control Block #" << pid << " Destructed." << endl;
 }
 
+//returns PID
 int ProcessControlBlock::getPID() {
     return pid;
 }
 
+//Initializes Page Table in Process Control Block
 void ProcessControlBlock::initializePageTable(int size) {
-    pageTable = new bool * [size];
+    pageTable = new int[size];
 }
 
-bool ** ProcessControlBlock::getPageTable() {
+//Returns Page Table stored in Process Control Block
+int * ProcessControlBlock::getPageTable() {
     return pageTable;
 }
 
+//Returns the size of the memory of the Process Control Block
 int ProcessControlBlock::getSize() {
     return processSize;
 }
@@ -120,9 +159,13 @@ int ProcessControlBlock::getSize() {
 
 //global variables
 MemoryBlockTable mem;
-list<ProcessControlBlock*> readyQueue;
-int counter = 0;
+vector<ProcessControlBlock*> readyQueue;
+int counter = 0; //Counter for unique id
 
+/*
+Menu function will display a menu and grab user input. Returns an integer of the 
+user input
+*/
 int menu() {
     int choice; //variable for user choice
     cout << endl << endl; //display menu
@@ -141,14 +184,13 @@ int menu() {
     return choice; //return user input
 }
 
+//Initialization function that will create a PCB and allocate Memory to MBT
 void initiate() {
     //Select random size from 1 - 30 to allocate dynamic memory
     int size = rand() % 30 + 1;
     
     //Create PCB
     ProcessControlBlock * pcb = new ProcessControlBlock(counter, size);
-    
-
     if(mem.getSpace() - size >= 0) { //check if there are memory space
         counter++; //increment unique id
         pcb->initializePageTable(size);
@@ -156,20 +198,23 @@ void initiate() {
         readyQueue.push_back(pcb);
 
         mem.setSpace(mem.getSpace() - size); //update space size
-
+        cout << "Memory Space: " << mem.getSpace() << endl;
     } else { //Not enough space in memory block table
-        delete pcb; //clear pointer
+        delete pcb; //delete PCB pointer
         cout << "Error: Insufficient free blocks are available in the Memory Block Table " << endl;
     }
 }
 
+//Prints out the PID of the Ready Queue
 void printReadyQueue() {
     //Go through list and display all PID
-    for(list<ProcessControlBlock*>::iterator it = readyQueue.begin(); it != readyQueue.end(); ++it) {
-        cout << "Ready Queue Process Block ID:" << (*it)->getPID() << endl;
+    for(int ii = 0; ii < readyQueue.size(); ii++) {
+        ProcessControlBlock * temp = readyQueue.at(ii);
+        cout << "Ready Queue Process Block ID: " << (temp)->getPID() << " |  Size: " << (temp)->getSize() << " Blocks" << endl;
     }
 }
 
+//User selects a Process to terminate based of PID
 void terminateProcess() {
     int choice;
     cout << "Terminate process PID: ";
@@ -178,18 +223,30 @@ void terminateProcess() {
 
     bool found = false;
     //Go through list
-    for(list<ProcessControlBlock*>::iterator it = readyQueue.begin(); it != readyQueue.end(); ++it) {
-        if((*it)->getPID() == choice) { //found pid in process
-            found = true;
-            mem.deallocateMemory((*it)->getPageTable(), (*it)->getSize());
+    for(int ii = 0; ii < readyQueue.size(); ii++) {
+        ProcessControlBlock * temp = &(*readyQueue[ii]); //get PCB
+        if((temp)->getPID() == choice) { //found pid in process
+            found = true; //boolean found
+            mem.deallocateMemory((temp)->getPageTable(), (temp)->getSize()); //take back memory
+            delete temp;
+            readyQueue.erase(readyQueue.begin() + ii); //Remove PCB from Ready Queue
         }
     }
 
-    if(!found) {
+    if(!found) { //If not found then display error
         cout << "Error: PID = " << choice << " not found." << endl;
     }
 }
 
+//Function will check ReadyQueue and delete any instance of any PCB inside readyQueue
+void exitProgram() {
+    for(int ii = 0; ii < readyQueue.size(); ii++) {
+        ProcessControlBlock * temp = &(*readyQueue[ii]); //create temporary variable to get PCB
+        delete temp; //Delete PCB
+    }
+}
+
+//Main start of function
 int main() {
     //Set variables
     bool exit = false;
@@ -208,15 +265,16 @@ int main() {
             case 3: //Terminate process with a speicific PID
                 terminateProcess();
                 break;
-            case 4:
+            case 4: //Exit program
                 exit = true;
+                exitProgram();
                 cout << "Exitting program" << endl;
                 break;
-            default:
+            default: //Catches any invalid inputs
                 cout << "Invalid selection" << endl;
         }
     }
 
+    
     return 0;
 }
-
